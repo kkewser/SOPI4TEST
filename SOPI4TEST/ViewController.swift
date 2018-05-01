eaa049be8f94821c35d9c23a91320ce8e5da05e5
Kewser <kkewser>
2018-05-01 21:41:56 +0200

    SOPI4TEST connect to database

--- SOPI4TEST/ViewController.swift
+++ SOPI4TEST/ViewController.swift
@@ -7,11 +7,22 @@
 //
 
 import UIKit
+import Firebase
+class ViewController: UIViewController, UITextFieldDelegate{
 
-class ViewController: UIViewController {
-
+    @IBOutlet weak var CaregiveName: UITextField!
+      var reference: DatabaseReference?
+    
+    @IBAction func Skickabuttom(_ sender: Any) {
+         reference?.child("Result").setValue(CaregiveName.text);
+        print(CaregiveName.text)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+         reference = Database.database().reference()
+        CaregiveName.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +30,10 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {   //delegate method
+        textField.resignFirstResponder()
+        return true
+    }
 
 }
 
